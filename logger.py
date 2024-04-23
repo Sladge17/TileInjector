a3f14248784daea474b7c58b5ec1939066916f38
Maxim Sosov <sladge17@gmail.com>
2024-04-23 15:45:32 +0300

    improve class Logger

--- logger.py
+++ logger.py
@@ -1,43 +1,43 @@
 class Logger:
     @classmethod
     def task_done(cls):
-        print("INFO: Task done", end="\n\n")
+        print("INFO: Success completed", end="\n\n")
 
 
     @classmethod
     def not_target_object(cls, obj: str, target_type: str):
-        print(f"WARNING: Object \"{obj}\" not a {target_type.lower()}")
+        print(f"\033[33mWARNING: Object \"{obj}\" not a {target_type.lower()}\033[37m")
 
 
     @classmethod
     def uv_more_than_need(cls, obj: str, channels: int):
-        print(f"WARNING: Object \"{obj}\" has uv more than {channels}, need optimize")
+        print(f"\033[33mWARNING: Object \"{obj}\" has uv more than {channels}, need optimize\033[37m")
 
 
     @classmethod
     def uv_less_than_need(cls, obj: str, channels: int):
-        print(f"WARNING: Object \"{obj}\" has uv less than {channels}")
+        print(f"\033[33mWARNING: Object \"{obj}\" has uv less than {channels}\033[37m")
 
 
     @classmethod
     def empty_sample(cls):
-        print("ERROR: No correct object selected")
+        print("\033[31mERROR: No correct object selected\033[37m")
 
 
     @classmethod
     def empty_path(cls, slot: str):
-        print(f"ERROR: Empty path in slot \"{slot}\"")
+        print(f"\033[31mERROR: Empty path in slot \"{slot}\"\033[37m")
     
     @classmethod
     def file_not_exist(cls, file: str, slot: str):
-        print(f"ERROR: File {file} not exist in slot \"{slot}\"")
+        print(f"\033[31mERROR: File {file} not exist in slot \"{slot}\"\033[37m")
 
 
     @classmethod
     def file_empty(cls, file: str, slot: str):
-        print(f"ERROR: File {file} is empty in slot \"{slot}\"")
+        print(f"\033[31mERROR: File {file} is empty in slot \"{slot}\"\033[37m")
 
 
     @classmethod
     def file_not_texture(cls, file: str, slot: str):
-        print(f"ERROR: File {file} not a texture in slot \"{slot}\"")
+        print(f"\033[31mERROR: File {file} not a texture in slot \"{slot}\"\033[37m")
