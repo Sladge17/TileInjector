d593f96c9f26877e8c15a5738904fd59bec704e8
Maxim Sosov <sladge17@gmail.com>
2024-04-24 16:48:35 +0300

    rename main.py to tile_injector.py

--- script_executor.py
+++ script_executor.py
@@ -2,11 +2,11 @@ import os
 
 
 script_path = "/home/maxim/Projects/LestaTest"
-script_name = "main.py"
+script_name = "tile_injector.py"
 
 os.environ['SCRIPT_PATH'] = script_path
 
-script_filename = os.path.join("/home/maxim/Projects/LestaTest", "main.py")
+script_filename = os.path.join(script_path, script_name)
 with open(script_filename) as text:
     script = text.read()
 
